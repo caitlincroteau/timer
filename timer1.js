9e7b389aebf7a967feb79c6b723562468e98dbaf
Caitlin <cjcroteau@gmail.com>
2022-04-16 01:18:17 +0000

    clean up comments in timer1.js

--- timer1.js
+++ timer1.js
@@ -1,19 +1,6 @@
 //alarm clock / timer which will beep after a specified amount of time has passed.
 //The user can specify an unlimited number of alarms using command line arguments
 
-//EXAMPLE:
-// node timer1.js 10 3 5 15 9
-
-// This will make it beep at:
-// 3 seconds
-// 5 seconds
-// 9 seconds
-// 10 seconds
-// 15 seconds
-
-//const delays = process.argv.slice(2);
-//[10, 3, 5, 15, 9]
-
 const timer = function() {
   const strDelays = process.argv.slice(2);
   const numDelays = strDelays.map(x => Number(x));
@@ -29,11 +16,13 @@ const timer = function() {
 
 timer();
 
-// EDGE CASES
 
-// No numbers are provided:
-//Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
-// An input is a negative number:
-//Ignore/skip any numbers that are negative. We can't schedule anything in the past.
-// An input is not a number:
-//Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
+//EXAMPLE:
+// node timer1.js 10 3 5 15 9
+
+// This will make it beep at:
+// 3 seconds
+// 5 seconds
+// 9 seconds
+// 10 seconds
+// 15 seconds
